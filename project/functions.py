586e3a9180b77b3a34c616dfa15431a5206ae62d
Marcos Aguayo <marcos@aguayo.es>
2019-03-01 17:27:43 +0000

    Add is_valid_token

--- project/functions.py
+++ project/functions.py
@@ -1,2 +1,18 @@
+import jwt
+from jwt.exceptions import InvalidSignatureError
+from django.conf import settings
+
 def response_wrapper(success, data, code=None):
     return {"success": success, "code": code, "data": data}
+
+def is_valid_token(token):
+    try:
+        decoded_token = jwt.decode(
+            token,
+            settings.JWT_AUTH["JWT_SECRET_KEY"],
+            algorithms=["HS256"],
+        )
+    except InvalidSignatureError:
+        return False
+    else:
+        return True
