112c81a76c80be7368bea51a6eb038303e1ea122
Marcos Aguayo <marcos@aguayo.es>
2019-03-02 00:29:01 +0000

    Linting

--- project/functions.py
+++ project/functions.py
@@ -2,15 +2,15 @@ import jwt
 from jwt.exceptions import InvalidSignatureError
 from django.conf import settings
 
+
 def response_wrapper(success, data, code=None):
     return {"success": success, "code": code, "data": data}
+
 
 def is_valid_token(token):
     try:
         decoded_token = jwt.decode(
-            token,
-            settings.JWT_AUTH["JWT_SECRET_KEY"],
-            algorithms=["HS256"],
+            token, settings.JWT_AUTH["JWT_SECRET_KEY"], algorithms=["HS256"]
         )
     except InvalidSignatureError:
         return False
